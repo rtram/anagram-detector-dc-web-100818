479c14259c3dadf59dd1feb4e021df9022513222
Robin Tram <robin.tram@gmail.com>
2018-10-13 14:53:13 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
   end 
   
   def match(array)
-    mod_array = array.collect! {|string| string.split(//).sort.join}
+    mod_array = array.collect {|string| string.split(//).sort.join}
     mod_string = self.word.split(//).sort.join
     mod_array.select {|word| word == mod_string}
   end 
