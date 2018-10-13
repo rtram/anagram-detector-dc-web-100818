98ff4df2ba5f0301263a2e7ac0c03f505c0bdd85
Robin Tram <robin.tram@gmail.com>
2018-10-13 15:07:01 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,10 +9,11 @@ class Anagram
   end 
   
   def match(array)
-    mod_array = array.each {|string| string.split(//).sort.join}
+    original = array
+    mod_array = array.collect {|string| string.split(//).sort.join}
     mod_string = self.word.split(//).sort.join
     indices = mod_array.each_index.select {|index| mod_array[index] == mod_string}
-    indices.map {|index| array[index]}
+    indices.map {|index| original[index]}
     binding.pry
   end 
   
