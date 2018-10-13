54a14d98935785cfb1375c09d7a63b85f916b7cf
Robin Tram <robin.tram@gmail.com>
2018-10-13 15:03:23 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,7 @@ class Anagram
     mod_array = array.collect {|string| string.split(//).sort.join}
     mod_string = self.word.split(//).sort.join
     indices = mod_array.each_index.select {|same| same == mod_string}
-    # binding.pry
+    binding.pry
   end 
   
   
