fc12e0c4014d4c65f9c3760765a70c490317d7d7
Robin Tram <robin.tram@gmail.com>
2018-10-13 15:08:56 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram 
+  
+  attr_reader :word
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(array)
+    original = array
+    mod_array = array.collect {|string| string.split(//).sort.join}
+    mod_string = self.word.split(//).sort.join
+    indices = mod_array.each_index.select {|index| mod_array[index] == mod_string}
+    if indices.length > 0 
+      indices.map {|index| original[index]}
+    else 
+      indices
+    end 
+    # binding.pry
+  end 
+  
+  
+end 
