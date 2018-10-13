c7defe6c7371316094e84346bdea46816e464b68
Robin Tram <robin.tram@gmail.com>
2018-10-13 14:41:33 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,9 +1,18 @@
+require 'pry'
+
 class Anagram 
   
+  attr_accessor :array_string
   attr_reader :word
   
   def initialize(word)
     @word = word 
+  end 
+  
+  def match(array)
+    array_string = array
+    binding.pry
+    self.word 
   end 
   
   
