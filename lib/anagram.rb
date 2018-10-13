7ad53f2ca867542e34a7718fbdec2c7608088829
Robin Tram <robin.tram@gmail.com>
2018-10-13 14:45:30 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,7 +2,6 @@ require 'pry'
 
 class Anagram 
   
-  attr_accessor :array_string
   attr_reader :word
   
   def initialize(word)
@@ -10,7 +9,7 @@ class Anagram
   end 
   
   def match(array)
-    self.array_string = array.collect! {|string| string.split.sort}
+    array.collect! {|string| string.split.sort}
     binding.pry
     self.word 
   end 
